e06a7e57fbf8849f3b821a1c7a0f29aa7db0624b
Anshul1563 <pesanshul@gmail.com>
2022-10-17 17:19:21 +0530

    cssTest

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import './styles.css'
 export default function App() {
     
     return (
       <div>
-          <p className="text-4xl bg-slate-900 text-white text-center p-2"> Testing Tailwind...</p>    
+          <p className="cssTest text-4xl bg-slate-900 text-center p-2">Testing Tailwind (Centered and Black bg) and CSS (Yellow Text)</p>    
       </div>
     )
   }
